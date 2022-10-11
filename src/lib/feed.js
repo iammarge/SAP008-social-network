643b8043aa92b4c4c70e248e362de692f0c44dc5
Tamyres <tamyresfmelo@gmail.com>
2022-10-11 18:40:48 -0300

    estilização da pag do feed

--- src/lib/feed.js
+++ src/lib/feed.js
@@ -4,21 +4,23 @@ export default () => {
     const containerFeed = document.createElement('div');
     containerFeed.classList.add('set-feed');
     const templateFeed = `
-    <nav class="nav-header">
-     <img class="google" src="img/logo.png" alt="logo Google">
+    <div class=div-feed>
+      <nav class="nav-header">
+        <img class="logo-feed" src="img/logo.png" alt="logo Google">
 
-     <div id="buscar"></div>
-      <input type="text" class="search-for" placeholder="Buscar..." />
-      <img src="img/lupa.png" id="btn-search" alt="Buscar" />
-     </div>
-    </nav>
-    <div>
-     <form class="form-message">
-      <textarea id="message" placeholder="Escreva sua publicação aqui..."></textarea>
-      <button type="submit" id="publish" class="btn-publish">Publicar</button>
-     </form>
-    </div>
-    <footer> Developed by: Marjorie Santos e Tamyres França.</footer>
+        <div id="buscar"></div>
+          <input type="text" class="search-for" placeholder="Buscar..." />
+          <img src="img/lupa.png" id="btn-search" alt="Buscar" />
+        </div>
+      </nav>
+      <div>
+      <form class="form-message">
+        <textarea id="message" placeholder="Escreva sua publicação aqui..."></textarea>
+        <button type="submit" id="publish" class="btn-publish">Publicar</button>
+      </form>
+      </div>
+      <footer> Developed by: Marjorie Santos e Tamyres França.</footer>
+    </div>  
 `;
     containerFeed.innerHTML = templateFeed;
   
