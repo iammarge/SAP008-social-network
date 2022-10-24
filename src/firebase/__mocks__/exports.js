cbf3eed3fd5a66e7bcd3ab385a42368fb186abc1
Marjorie Santos <72046467+iammarge@users.noreply.github.com>
2022-10-24 13:33:15 -0300

    alteração mocks

--- src/firebase/__mocks__/exports.js
+++ src/firebase/__mocks__/exports.js
@@ -1,4 +1,8 @@
 export const initializeApp = jest.fn();
 export const getAuth = jest.fn();
 export const createUserWithEmailAndPassword = jest.fn();
+export const signInWithEmailAndPassword = jest.fn();
+export const signInWithPopup = jest.fn();
+export const GoogleAuthProvider = jest.fn();
+export const updateProfile = jest.fn();
 export const getFirestore = jest.fn();
