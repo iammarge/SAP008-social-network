f7d8305ddc6fd4746f32b92e2083730a8692a135
Marjorie Santos <72046467+iammarge@users.noreply.github.com>
2022-11-22 13:05:21 -0300

    testes unitários

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,15 @@
-import { login } from '../src/firebase/auth.js';
-import { signInWithEmailAndPassword } from '../src/firebase/exports.js';
+import {
+  login,
+  signIn,
+  signInGoogle,
+  logout,
+} from '../src/firebase/auth.js';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from '../src/firebase/exports.js';
 
 jest.mock('../src/firebase/exports.js');
 
@@ -7,5 +17,27 @@ describe('loginEmailAndPassword', () => {
   it(' a função deve logar um usuario utilizando email e senha', () => {
     login('teste@teste.com', '123456');
     expect(signInWithEmailAndPassword).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('createUser', () => {
+  it('a função deve criar um novo usuario com email e senha', async () => {
+    createUserWithEmailAndPassword.mockResolvedValue({});
+    await signIn();
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('loginGoogle', () => {
+  it(' a função deve fazer login com a conta do Google ', () => {
+    signInGoogle();
+    expect(signInWithPopup).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('userLogout', () => {
+  it(' a função deve fazer logout da conta ', () => {
+    logout();
+    expect(signOut).toHaveBeenCalledTimes(1);
   });
 });
